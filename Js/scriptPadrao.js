c5a036edb4664976b9b2317f1697c8db8998abd2
Nicolas de Almeida Sousa <121115391+NicolasStudio@users.noreply.github.com>
2025-11-09 19:12:24 -0300

    Adaptação de outros desenvolvimentos
    Inclusos nesta home de jogos, dois projetos (Batalha naval e Jogo da memória), afim mesclar e facilitar a experiência do usuário.

---
+++ Js/scriptPadrao.js
@@ -0,0 +1,11 @@
+function home(){
+    window.location.href = "index.html";
+}
+
+function abrirBatalhaNaval(){
+    window.location.href = "batalhaNaval.html";
+}
+
+function abrirJogoMemoria(){
+    window.location.href = "jogoMemoria.html";
+}
